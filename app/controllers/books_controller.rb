5ed47e5cd159ea70ab7615b4ac38aedeb63a35aa
jason <jason@example.com>
2017-01-24 00:49:12 +0800

    Refine Web

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   before_action :set_book, :only => [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).per(5)
   end
 
   def new
